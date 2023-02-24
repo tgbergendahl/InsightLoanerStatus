707265fffc0fbd8e9508dd30b535799e40980ad4
ajn15104 <122473317+ajn15104@users.noreply.github.com>
2023-02-24 13:10:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ workspaceId = "b05cdfec-a5a3-44ee-b42b-733584cd353d"
 
 url = f"https://api.atlassian.com/jsm/assets/workspace/{workspaceId}/v1/aql/objects?objectSchema='Personal Computing'"
 
-headers = {"Authorization": "Basic dGhiMTkwMDd3b3JrQHVjb25uLmVkdTpBVEFUVDN4RmZHRjAwRW1nUlFKOUhNUTZzSklUVWdpMFd3Z2piWXhpdU1HTjZ6UFMtWmZhZVVYcjloZXljUGpVcTBqdE9hZXBYbTJnYXRsVzk5SFo1Nkh0X1h4Szl0dG1XUWM5RXhfWnFsUDhyMjUzZmlVMUljazh2OUtqb25xLWxDMzVTekF1enQ2OFlwTFA0ejlPME0tdmRuTzh5Wnl3SzBXWFhuaWNXLThSRG1PYnhqTFc1QU09REFDQkY0M0M",
+headers = {"Authorization": "Basic {key}",
            "Accept": "application/json"}
 
 response = requests.request("GET", url, headers=headers)
