a76c7e9c547eea1a2370f511646f195c64d3b383
tgbergendahl <tgbergendahl@outlook.com>
2023-02-24 12:05:15 -0500

    figuring out how to parse data

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import json
 
 workspaceId = "b05cdfec-a5a3-44ee-b42b-733584cd353d"
 
-url = f"https://api.atlassian.com/jsm/assets/workspace/{workspaceId}/v1/aql/objects"
+url = f"https://api.atlassian.com/jsm/assets/workspace/{workspaceId}/v1/aql/objects?objectSchema='Personal Computing'"
 
 headers = {"Authorization": "Basic dGhiMTkwMDd3b3JrQHVjb25uLmVkdTpBVEFUVDN4RmZHRjAwRW1nUlFKOUhNUTZzSklUVWdpMFd3Z2piWXhpdU1HTjZ6UFMtWmZhZVVYcjloZXljUGpVcTBqdE9hZXBYbTJnYXRsVzk5SFo1Nkh0X1h4Szl0dG1XUWM5RXhfWnFsUDhyMjUzZmlVMUljazh2OUtqb25xLWxDMzVTekF1enQ2OFlwTFA0ejlPME0tdmRuTzh5Wnl3SzBXWFhuaWNXLThSRG1PYnhqTFc1QU09REFDQkY0M0M",
            "Accept": "application/json"}
 
 response = requests.request("GET", url, headers=headers)
 
+objectData = json.dumps(json.loads(response.text),
+                        sort_keys=True, indent=4, separators=(",", ": "))
 
-print(json.dumps(json.loads(response.text),
-      sort_keys=True, indent=4, separators=(",", ": ")))
+f = open("out.txt", "w")
+f.write(objectData)
+f.close()
