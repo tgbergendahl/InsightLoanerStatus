c9ed81a1f47b3b897d2969908e0dd2caabc2c9d8
tgbergendahl <tgbergendahl@outlook.com>
2023-02-27 08:26:38 -0500

    put key in separate file

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import json
 
 workspaceId = "b05cdfec-a5a3-44ee-b42b-733584cd353d"
 
-url = f"https://api.atlassian.com/jsm/assets/workspace/{workspaceId}/v1/aql/objects?objectSchema='Personal Computing'"
+fkey = open("key.txt", "r")
+key = fkey.readline()
 
-headers = {"Authorization": "Basic {key}",
+url = f"https://api.atlassian.com/jsm/assets/workspace/{workspaceId}/v1/aql/objects?objectSchema='Personal Computing'&resultPerPage=99"
+
+headers = {"Authorization": f"Basic {key}",
            "Accept": "application/json"}
 
 response = requests.request("GET", url, headers=headers)
